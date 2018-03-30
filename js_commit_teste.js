e8b9375eacc4851aecd8b7622d8a4f80c02a0781
hugolpa <37938486+hugolpa@users.noreply.github.com>
2018-03-30 15:50:38 -0300

    coreção ortografica
    coreção ortografica no comentário

--- js_commit_teste.js
+++ js_commit_teste.js
@@ -1,3 +1,3 @@
 
 var x ="volvo"+16+4; //está juntando a string volvo com o numero 16 juntado com o numero 4
-alert(x);//mostra a var riavel x dentro de um alert
+alert(x);//mostra a váriavel x dentro de um alert
